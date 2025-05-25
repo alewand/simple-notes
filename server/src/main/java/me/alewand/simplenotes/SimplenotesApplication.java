c25489ebf3a18f6f525ca2a9b4a1d782e6011300
alewand <lewandowskiamadeusz2004@gmail.com>
2025-05-25 22:13:14 +0200

    Init.

---
+++ server/src/main/java/me/alewand/simplenotes/SimplenotesApplication.java
@@ -0,0 +1,13 @@
+package me.alewand.simplenotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplenotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplenotesApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/me/alewand/simplenotes/SimplenotesApplicationTests.java
@@ -0,0 +1,13 @@
+package me.alewand.simplenotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplenotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
